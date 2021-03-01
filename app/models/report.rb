c06b3c0bf0b4bd36d02f7a615b7a461d1a577701
Adnilson <adnilson.delgado@outlook.com>
2021-03-01 23:28:23 +0000

    Add Report validations

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
+
+RESULT_REGEX = /\A(approved|rejected)\z/
+
 class Report < ApplicationRecord
   has_one_attached :image
 
-  after_update :send_report_result
+  validates :user_id, :caller_url, presence: true, on: :create
 
-
-  def send_report_result
-    SendReportWorker.perform_async(id)
-  end
+  validates_format_of :result, with: RESULT_REGEX, on: :update
 end
