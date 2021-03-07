a13f0ea14b52e47abb205d6d5a7de6a165c5562b
Adnilson <adnilson.delgado@outlook.com>
2021-03-07 22:19:49 +0000

    Add basic authentication to reports moderation

--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -1,25 +1,33 @@
 require "test_helper"
+require "sidekiq/testing"
 
 class ReportsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @headers = {
+      'Authorization': ActionController::HttpAuthentication::Basic
+        .encode_credentials(ENV['REPORTS_MODERATOR'], ENV['REPORTS_PASSWORD'])
+    }
+  end
+
   test "should get index" do
-    get reports_url
+    get reports_url, headers: @headers
     assert_response :success
   end
 
-  test "should get update and approve" do
-    patch report_url(Report.last), params: { report: { result: 'approved' } }
+  test "should update when report's decision is approved" do
+    patch report_url(Report.last), headers: @headers, params: { report: { result: 'approved' } }
 
     assert_redirected_to reports_url
   end
 
-  test "should get update and reject" do
-    patch report_url(Report.last), params: { report: { result: 'rejected' } }
+  test "should update when report's decision is rejected" do
+    patch report_url(Report.last), headers: @headers, params: { report: { result: 'rejected' } }
 
     assert_redirected_to reports_url
   end
 
-  test "should get update and fail" do
-    patch report_url(Report.last), params: { report: { result: '' } }
+  test "should redirect to edit when no params are passed" do
+    patch report_url(Report.last), headers: @headers, params: { report: { result: '' } }
 
     assert_redirected_to edit_report_url(Report.last)
   end
